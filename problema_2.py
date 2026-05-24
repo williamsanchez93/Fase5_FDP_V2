ad6d7c4d62d12368b6e6b8eda6ec427da0d71156
williamsanchez93 <willymusikl@gmail.com>
2026-05-23 23:47:30 -0500

    Agregar comentarios

--- problema_2.py
+++ problema_2.py
@@ -1,17 +1,27 @@
-# =====================================================================
-# Curso: Fundamentos de Programación (Código: 213022)
-# Fase 5 - Evaluación Final POA
-# Problema Seleccionado: Problema #2 (Menú de Restaurante con Matriz)
-# Programa: Ingeniería de Sistemas
-# Universidad Nacional Abierta y a Distancia (UNAD)
-# =====================================================================
-
+#Nombre estudiante: William Abraham Sanchez Obando
+#Grupo: 213022_514
+#Programa ejercicio #2 - Matrices y Arreglos
+#Código Fuente: autoría propia
+#Fase 5: Evaluación Final POA
+#Curso: Fundamentos de Programación
+#Tutor: Flor María Hernández Perez
+'''Problema 2: Se gestionan los precios de un menú de restaurante. El menú se representa como una matriz: [Nombre del Producto, Categoría, Precio Base].
+Se requiere una funcionalidad para aplicar una promoción a productos específicos.
+Requisitos de Desarrollo
+-
+Matriz: Crear una matriz con al menos 6 productos de diversas categorías.
+-
+Módulos: Se requiere un módulo (función) para calcular el precio final de un producto.
+-
+Lógica de Negocio:
+✓
+Aplicar un 15% de descuento si el producto cumple con la categoría objetivo, específica y su precio base es mayor a un umbral definido.
+✓
+Mantener el precio base si no se cumplen las condiciones.
+-
+Salida: Mostrar cada producto, su precio base y el precio final con la promoción aplicada.
+'''
 def calcular_precio_final(categoria_producto, precio_base, categoria_objetivo, umbral_precio):
-    """
-    Módulo (función) que aplica la lógica de negocio.
-    Retorna el precio con un 15% de descuento si cumple los criterios,
-    de lo contrario retorna el precio base original.
-    """
     
     if categoria_producto.lower() == categoria_objetivo.lower() and precio_base > umbral_precio:
         descuento = precio_base * 0.15
